00ccaeb21d1d3b10ac56d94bda7f04011277a0e8
sohailB <bhattisohail20@gmail.com>
2023-07-18 18:06:16 +0500

    code updated

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,22 @@
-import Head from "next/head";
+import dynamic from "next/dynamic";
+const Routers = dynamic(() => import("./routers"));
 
 export default function App(props) {
-  switch (props.wildcard) {
-    case "home":
-      return (
-        <div>
-          Welcome to the Aussie-izer! This only works for .com domains. If you
-          want to Aussie-ize{" "}
-          <a href="https://example.com">https://example.com</a> visit{" "}
-          <a href="https://example.aussieizer.sampoder.com">
-            https://example.aussieizer.sampoder.com
-          </a>
-          .{" "}
-        </div>
-      );
-      break;
-    default:
-      return (
-        <>
-          <iframe
-            src={`https://${props.wildcard}.com`}
-            style={{
-              transform: "rotate(180deg)",
-              border: "none",
-              height: "100vh",
-              width: "100%",
-              overflow: "hidden",
-            }}
-            frameBorder="0"
-            scrolling="yes"
-            seamless="seamless"
-            height="100%"
-            width="100%"
-          ></iframe>
-          <style>
-            {`body {
-              margin: 0;
-            }`}
-          </style>
-        </>
-      );
-  }
+  return <Routers wildcard={props.wildcard} />;
 }
 
 export async function getServerSideProps(context) {
   let wildcard = context.req.headers.host.split(".")[0];
-  wildcard =
-    wildcard != "aussieizer"
-      ? wildcard != "localhost:3000"
-        ? wildcard
-        : process.env.TEST_WILDCARD
-      : "home";
-  return { props: { wildcard } };
+  if (!wildcard) {
+    wildcard = "home"; // Set a default value for the wildcard if it's not present in the request headers
+  } else {
+    wildcard =
+      wildcard !== "aussieizer"
+        ? wildcard !== "localhost:3000"
+          ? wildcard
+          : process.env.TEST_WILDCARD
+        : "home";
+  }
+
+  return { props: { wildcard: String(wildcard) } }; // Convert the wildcard to a string to ensure it's serializable
 }
